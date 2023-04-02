d8a0f5599cbb79177162d9be84f914a52e6addd0
Nanda Kumar <65768970+NandaKumar-cyber@users.noreply.github.com>
2023-04-02 14:20:00 +0530

    episode 3 commit

--- App.js
+++ App.js
@@ -7,20 +7,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div', { id: 'parent' },
- [React.createElement('div', { id: "child1" }, 
- [React.createElement('h1', { id: "sibling1" }, "Nested Heading 1 from child 1 "),
- React.createElement('h2', { id: 'sibling2'},"Nested Heading 2 from child 1 ")]
- ),React.createElement('div', { id: "child2" }, 
- [React.createElement('h1', { id: "sibling1" }, "Nested Heading 1 from child 2"),
- React.createElement('h2', { id: 'sibling2'},"Nested Heading 2 from child 2")]
- )])
+// const parent = React.createElement('div', { id: 'parent' },
+//  [React.createElement('div', { id: "child1" }, 
+//  [React.createElement('h1', { id: "sibling1" }, "Nested Heading 1 from child 1 "),
+//  React.createElement('h2', { id: 'sibling2'},"Nested Heading 2 from child 1 ")]
+//  ),React.createElement('div', { id: "child2" }, 
+//  [React.createElement('h1', { id: "sibling1" }, "Nested Heading 1 from child 2"),
+//  React.createElement('h2', { id: 'sibling2'},"Nested Heading 2 from child 2")]
+//  )])
 
 
 
 
 // const heading = React.createElement("h1", { id: "heading" }, "Vanakam React from app.js");
 
+const JsxHeading  = () =>( <h1 id="heading">This is from JSX</h1>
+)
+
+const HeadingComponent = () => {
+    return(
+    <div>
+        <JsxHeading />
+        <h1 id="heading">This is React Functional Component</h1>
+    </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<HeadingComponent />)
